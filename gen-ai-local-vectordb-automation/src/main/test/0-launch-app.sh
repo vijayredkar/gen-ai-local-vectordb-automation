e5b6c9bf7eab148d93dfd561ee5acea5dae2c8d1
Vijay Redkar <vijay0512@yahoo.com>
2025-01-01 09:58:59 -0800

    Delete gen-ai-local-vectordb-automation directory

--- gen-ai-local-vectordb-automation/src/main/test/0-launch-app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo
-echo building application
-cd /c/Vijay/Java/projects/openapi-ai-trials/LLM-gen-ai/langchain4j/gen-ai-local-vectordb-automation
-mvn clean install
-
-echo
-echo launching application
-java -jar target/gen-ai-local-vectordb.jar Standalone-LOCAL-MC internal
